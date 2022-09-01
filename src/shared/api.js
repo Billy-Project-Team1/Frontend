c6a7ea25ac46def031315f4434dd15d30b4c93ce
Yoonwon <blacksyw@naver.com>
2022-09-01 14:34:25 +0900

    WIP: Login/SignUp
    🚑: 기존 Slice를 이용한 Login/SignUp 방식에서 api.js를 이용하여 작업 진행
    🎨: 기존 백엔드 코드가 노출된것을 env환경 설정하여 바꿔놓음
    🔥: 기존 memberSlice 삭제
    🚧: alert 창 개선 필요(추후 디자인 완료시)

---
+++ src/shared/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+const token = localStorage.getItem("token");
+api.defaults.headers.common["authorization"] = token ? `${token}` : null;
+
+export const apis = {
+  // 회원 가입 API
+  createMember: (payload) =>
+    api.post("/members/signup", {
+      email: payload.email,
+      nickname: payload.nickname,
+      password: payload.password,
+    }),
+
+  //이메일 체크
+  checkEmailDuplicate: async (payload) =>
+    await api.get("/members/email-check", { params: { email: payload } }),
+
+  loginMember: (payload) =>
+    api.post("/members/login", {
+      email: payload.email,
+      password: payload.password,
+    }),
+};
