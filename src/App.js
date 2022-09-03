be56bb909b17a0a67e89379551655bff8398e982
Eunkyoung Kim <gina.kim.4130@gmail.com>
2022-09-03 22:35:29 +0900

    🚧 calendar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import "./App.scss";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+import ModifyPosting from './pages/modifyPosting/ModifyPosting';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<div>메인페이지</div>} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/modifyposting" element={<ModifyPosting />} />
     </Routes>
   );
 }
