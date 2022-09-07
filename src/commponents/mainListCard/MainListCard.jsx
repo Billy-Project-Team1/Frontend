3990f8a5e20427b8161b352c5c91fc70ecfe609a
Yoonwon <blacksyw@naver.com>
2022-09-07 21:28:13 +0900

    📰Feat:MainListCard 제작
    📰Feat:MainListCard 컴포넌트 제작 완료

--- src/commponents/mainListCard/MainListCard.jsx
+++ src/commponents/mainListCard/MainListCard.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 import './MainListCard.scss';
+import dailycost from '../../static/image/dailycost.svg';
+import deposit from '../../static/image/deposit.svg';
 import carrotImg from '../../static/image/carrotImg.jpg';
+import { FaStar } from 'react-icons/fa';
 
 const MainListCard = () => {
   return (
-    <div className="MainListCardContainer">
-      <img className="MainListCardImg" src={carrotImg} />
-      <div className="MainListCardRightBox">
-        <div className="MainListCardTitle">
-          아무튼 내가 최고 최고심 마우스 패드
-        </div>
-        <div className="MainListCardPlace">서울시 서초구 양재동</div>
-        <div>
-            
+    <div className="MainListWrap">
+      <div className="MainListCardContainer">
+        <div className="MainListCardBox">
+          <img className="MainListCardImg" src={carrotImg} />
+          <div className="MainListCardRightContainer">
+            <div className="MainListCardRightBox">
+              <div className="MainListCardTitle">
+                <div>코스트코 책장</div>
+              </div>
+              <div className="MainListCardPlace">서울시 서초구 양재동</div>
+              <div className="MainListCardBottomBox">
+                <div className="MainListCardPriceBox">
+                  <div className="MainListCardIconBox">
+                    <img className="MainListCardIcon" src={dailycost} />
+                  </div>
+                  49,000원
+                </div>
+                <div className="MainListCardPriceBox">
+                  <div className="MainListCardIconBox">
+                    <img className="MainListCardIcon" src={deposit} />
+                  </div>
+                  100,000원
+                </div>
+                <div className="MainListCardReview">
+                  <div className="MainListCardStarBox">
+                    <FaStar className="MainListCardStar" />
+                  </div>
+                  <div>5.0</div>
+                  &nbsp; 
+                  <div>(1)</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
