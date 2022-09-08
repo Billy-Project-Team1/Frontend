a6066ca0b87aa218708f3828f6c6dde4a17233be
Yoonwon <blacksyw@naver.com>
2022-09-09 02:46:38 +0900

    ✨Feat:무한스크롤 적용
    ✨Feat:메인페이지 List 불러올 때 무한 스크롤 기능 적용 완료

--- src/commponents/mainListCard/MainListCard.jsx
+++ src/commponents/mainListCard/MainListCard.jsx
@@ -5,45 +5,48 @@ import deposit from '../../static/image/deposit.svg';
 import carrotImg from '../../static/image/carrotImg.jpg';
 import { FaStar } from 'react-icons/fa';
 
-const MainListCard = () => {
+const MainListCard = ({ post }) => {
+  const postPrice = post.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  const postDeposit = post.deposit
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+
   return (
     <div className="MainListWrap">
       <div className="MainListCardContainer">
         <div className="MainListCardBox">
-          <img className="MainListCardImg" src={carrotImg} />
+          <img className="MainListCardImg" src={post.imgUrl} />
           <div className="MainListCardRightContainer">
             <div className="MainListCardRightBox">
               <div className="MainListCardTitle">
-                <div>
-                  아무튼 내가 최고 최고심 마우스패드 저는 이이이이이만큼
-                  나왔으면 해요
-                </div>
+                <div>{post.title}</div>
               </div>
-              <div className="MainListCardPlace">서울시 서초구 양재동</div>
+              <div className="MainListCardPlace">{post.location}</div>
               <div className="MainListCardBottomBox">
                 <div className="MainListCardPriceBox">
                   <div className="MainListCardIconBox">
                     <img className="MainListCardIcon" src={dailycost} />
                   </div>
-                  49,000원
+                  {postPrice}원
                 </div>
                 <div className="MainListCardPriceBox">
                   <div className="MainListCardIconBox">
                     <img className="MainListCardIcon" src={deposit} />
                   </div>
-                  100,000원
+                  {postDeposit}원
                 </div>
-              
               </div>
               <div className="MainListCardReview">
-                  <div className="MainListCardStarBox">
-                    <FaStar className="MainListCardStar" />
-                  </div>
-                  &nbsp;
-                  <div>5.0</div>
-                  &nbsp;
-                  <div>(1)</div>
+                <div className="MainListCardStarBox">
+                  <FaStar className="MainListCardStar" />
                 </div>
+                &nbsp;
+                <div>{post.avg}</div>
+                &nbsp;
+                <div>({post.reviewCount})</div>
+              </div>
             </div>
           </div>
         </div>
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import member from './modules/memberSlice';
 import myprofile from './modules/profileSlice';
+import posts from './modules/postsSlice';
 
 export default configureStore({
-  reducer: { member, myprofile },
+  reducer: { member, myprofile, posts },
 });
