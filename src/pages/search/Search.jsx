2588cde2622bcf4bc3ef77056e7df08aa7a14c07
4775614 <4775614@naver.com>
2022-09-15 05:02:26 +0900

    💄Style:Posting, Detail, Main, Search 스타일 수정

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -4,7 +4,7 @@ import { HiOutlineChevronLeft } from 'react-icons/hi';
 import './Search.scss';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { BsXCircle } from 'react-icons/bs';
+import { FiX } from 'react-icons/fi';
 import {
   getsearchPostsList,
   onRemoveHandler,
@@ -33,49 +33,49 @@ const Search = () => {
   const searchPostList = useSelector((state) => state.searchPost.postsList);
 
   return (
-    <>
-      <div className="SearchWrap">
-        <form className="SearchHeader" onSubmit={onSubmitHandler}>
-          <div className="SearchHeaderIconBox">
-            <HiOutlineChevronLeft
-              className="SearchHeaderIcon"
-              onClick={() => navigate(-1)}
-            />
-          </div>
-          <input
-            className="SearchHeaderInput"
-            placeholder="검색어를 입력해주세요."
-            onChange={onChange}
-            value={inputText}
-          />
-          {inputText === '' ? (
-            ''
-          ) : (
-            <BsXCircle
-              className="SearchHeaderInputXbutton"
-              onClick={() => {
-                onResetButton();
-              }}
-            />
-          )}
-        </form>
-      </div>
+		<>
+			<div className="SearchWrap">
+				<form className="SearchHeader" onSubmit={onSubmitHandler}>
+					<div className="SearchHeaderIconBox">
+						<HiOutlineChevronLeft
+							className="SearchHeaderIcon"
+							onClick={() => navigate(-1)}
+						/>
+					</div>
+					<input
+						className="SearchHeaderInput"
+						placeholder="검색어를 입력해주세요."
+						onChange={onChange}
+						value={inputText}
+					/>
+					{inputText === '' ? (
+						''
+					) : (
+						<FiX
+							className="SearchHeaderInputXbutton"
+							onClick={() => {
+								onResetButton();
+							}}
+						/>
+					)}
+				</form>
+			</div>
 
-      {searchPostList.length === 0 ? (
-        <div className="SearchEmptyText">
-          지역명 + 제품명으로 검색하여 필요한 제품을 찾아보세요! 예) 강남
-          자전거, 분당 노트북
-        </div>
-      ) : (
-        <div className="SearchPostList">
-          {searchPostList.map((post) => {
-            return <MainListCard post={post} key={post.id} />;
-          })}
-        </div>
-      )}
-      <Footer />
-    </>
-  );
+			{searchPostList.length === 0 ? (
+				<div className="SearchEmptyText">
+					지역명 + 제품명으로 검색하여 필요한 제품을 찾아보세요! <br/> 예) 강남
+					자전거, 분당 노트북
+				</div>
+			) : (
+				<div className="SearchPostList">
+					{searchPostList.map((post) => {
+						return <MainListCard post={post} key={post.id} />;
+					})}
+				</div>
+			)}
+			<Footer />
+		</>
+	);
 };
 
 export default Search;
