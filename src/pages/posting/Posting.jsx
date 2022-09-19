bfa07629e73cb2a0bf6e922c9ac6d69f09bf889e
Yoonwon <blacksyw@naver.com>
2022-09-19 18:23:36 +0900

    🚑Fix:포스팅 페이지 scroll 수정
    🚑Fix:포스팅 페이지 scroll 수정

--- src/pages/posting/Posting.jsx
+++ src/pages/posting/Posting.jsx
@@ -30,78 +30,82 @@ import PostingMap from '../../commponents/maps/PostingMap';
 import { addPost } from '../../redux/modules/postSlice';
 
 const Posting = () => {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const [searchMapModal, setSearchMapModal] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [searchMapModal, setSearchMapModal] = useState(false);
 
-	const [calendarOpen, setCalendarOpen] = useState(false);
-	const calendarClose = () => {
-		setCalendarOpen(!calendarOpen);
-	};
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-	/////////////////
+  const [calendarOpen, setCalendarOpen] = useState(false);
+  const calendarClose = () => {
+    setCalendarOpen(!calendarOpen);
+  };
 
-	const initialState = {
-		title: '',
-		content: '',
-		price: '',
-		deposit: '',
-		location: '',
-		detailLocation: '',
-		latitude: '',
-		longitude: '',
-		// blockDateDtoList: {},
-		// files: form/data,
-	};
-	const [data, setData] = useState(initialState);
-	const [blockDateDtoList, setBlockDateDtoList] = useState([]);
-	const [img, setImg] = useState([]); // file
+  /////////////////
 
-	const onChangeHandler = (e) => {
-		const { name, value } = e.target;
-		//...data 기존 데이터 두고 추가시키는 느낌~ㅋㅋ
-		setData({ ...data, [name]: value });
-	};
-	console.log(data);
-	console.log(blockDateDtoList.blockDateDtoList);
+  const initialState = {
+    title: '',
+    content: '',
+    price: '',
+    deposit: '',
+    location: '',
+    detailLocation: '',
+    latitude: '',
+    longitude: '',
+    // blockDateDtoList: {},
+    // files: form/data,
+  };
+  const [data, setData] = useState(initialState);
+  const [blockDateDtoList, setBlockDateDtoList] = useState([]);
+  const [img, setImg] = useState([]); // file
 
-	/////////////////
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    //...data 기존 데이터 두고 추가시키는 느낌~ㅋㅋ
+    setData({ ...data, [name]: value });
+  };
+  console.log(data);
+  console.log(blockDateDtoList.blockDateDtoList);
 
-	//   //[1] post
-	const onPostingHandler = async (e) => {
-		e.preventDefault();
-		//이미지 form 데이터
-		let formData = new FormData();
-		//a는 이름으로  b를 저장한다. c는 어떠한 타입으로 / form은 c를 굳이 안써도됨
-		// formData.append(a,b)
-		formData.append(
-			'postUploadRequestDto',
-			new Blob([JSON.stringify(data)], { type: 'application/json' })
-		);
-		for (let i = 0; i < img.length; i++) {
-			formData.append('files', img[i]);
-		}
-		for (let i = 0; i < blockDateDtoList.blockDateDtoList.length; i++) {
-			formData.append('blockDateDtoList', blockDateDtoList.blockDateDtoList[i]);
-		}
-		try {
-			const data = await dispatch(addPost(formData)).unwrap();
-			console.log(data);
-			if (data) {
-				window.location.replace('/');
-				window.location.replace(`/detail/${data.id}`);
-			} else {
-				console.log(data);
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  /////////////////
 
-	return (
-		<div>
-			<AddPostingHeader onPostingHandler={onPostingHandler} />
-			{/* <AddLocationHeader></AddLocationHeader>
+  //   //[1] post
+  const onPostingHandler = async (e) => {
+    e.preventDefault();
+    //이미지 form 데이터
+    let formData = new FormData();
+    //a는 이름으로  b를 저장한다. c는 어떠한 타입으로 / form은 c를 굳이 안써도됨
+    // formData.append(a,b)
+    formData.append(
+      'postUploadRequestDto',
+      new Blob([JSON.stringify(data)], { type: 'application/json' })
+    );
+    for (let i = 0; i < img.length; i++) {
+      formData.append('files', img[i]);
+    }
+    for (let i = 0; i < blockDateDtoList.blockDateDtoList.length; i++) {
+      formData.append('blockDateDtoList', blockDateDtoList.blockDateDtoList[i]);
+    }
+    try {
+      const data = await dispatch(addPost(formData)).unwrap();
+      console.log(data);
+      if (data) {
+        window.location.replace('/');
+        window.location.replace(`/detail/${data.id}`);
+      } else {
+        console.log(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <AddPostingHeader onPostingHandler={onPostingHandler} />
+      {/* <AddLocationHeader></AddLocationHeader>
 			<ModifyPostingHeader></ModifyPostingHeader>
 			<ModifyProfileHeader></ModifyProfileHeader>
 			<MypageHeader></MypageHeader>
@@ -109,86 +113,86 @@ const Posting = () => {
 			<DetailHeader></DetailHeader>
 			<MainHeader></MainHeader> */}
 
-			<div className="posting_container">
-				<div className="posting_image">
-					<ImageUploader img={img} setImg={setImg} />
-				</div>
-				<div className="posting_title">
-					<input
-						type="text"
-						placeholder="제품명"
-						//input에 네임, 밸류 설정하기.
-						name="title"
-						value={data.title}
-						onChange={onChangeHandler}
-					/>
-				</div>
-				<div className="posting_rental">
-					<div className="posting_price">
-						<label className="posting_price_label">일 대여금</label>
-						<input
-							onInput={(e) => {
-								if (e.target.value.length > e.target.maxLength)
-									e.target.value = e.target.value.slice(0, e.target.maxLength);
-							}}
-							className="posting_price_input"
-							type="number"
-							placeholder="￦"
-							name="price"
-							value={data.price}
-							onChange={onChangeHandler}
-							maxlength={9}
-						/>
-					</div>
+      <div className="posting_container">
+        <div className="posting_image">
+          <ImageUploader img={img} setImg={setImg} />
+        </div>
+        <div className="posting_title">
+          <input
+            type="text"
+            placeholder="제품명"
+            //input에 네임, 밸류 설정하기.
+            name="title"
+            value={data.title}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div className="posting_rental">
+          <div className="posting_price">
+            <label className="posting_price_label">일 대여금</label>
+            <input
+              onInput={(e) => {
+                if (e.target.value.length > e.target.maxLength)
+                  e.target.value = e.target.value.slice(0, e.target.maxLength);
+              }}
+              className="posting_price_input"
+              type="number"
+              placeholder="￦"
+              name="price"
+              value={data.price}
+              onChange={onChangeHandler}
+              maxlength={9}
+            />
+          </div>
 
-					<div className="posting_deposit">
-						<label className="posting_deposit_label">보증금</label>
-						<input
-							onInput={(e) => {
-								if (e.target.value.length > e.target.maxLength)
-									e.target.value = e.target.value.slice(0, e.target.maxLength);
-							}}
-							className="posting_deposit_input"
-							type="number"
-							placeholder="￦"
-							name="deposit"
-							value={data.deposit}
-							onChange={onChangeHandler}
-							maxlength={9}
-							// onChange={e => form({setDeposit: e.target.value.replace(/[^0-9]/g, "")})} />
-						/>
-					</div>
-				</div>
+          <div className="posting_deposit">
+            <label className="posting_deposit_label">보증금</label>
+            <input
+              onInput={(e) => {
+                if (e.target.value.length > e.target.maxLength)
+                  e.target.value = e.target.value.slice(0, e.target.maxLength);
+              }}
+              className="posting_deposit_input"
+              type="number"
+              placeholder="￦"
+              name="deposit"
+              value={data.deposit}
+              onChange={onChangeHandler}
+              maxlength={9}
+              // onChange={e => form({setDeposit: e.target.value.replace(/[^0-9]/g, "")})} />
+            />
+          </div>
+        </div>
 
-				<div className="posting_content">
-					<textarea
-						type="text"
-						placeholder="게시물 내용을 작성해주세요. (적절하지 못한 제품은 게시가 제한될 수
+        <div className="posting_content">
+          <textarea
+            type="text"
+            placeholder="게시물 내용을 작성해주세요. (적절하지 못한 제품은 게시가 제한될 수
 				있어요.)"
-						name="content"
-						value={data.content}
-						onChange={onChangeHandler}
-					/>
-				</div>
-				<div className="posting_calendar_wrap">
-					<div className="posting_calendar_icon">
-						<Calendar setData={setBlockDateDtoList} data={blockDateDtoList} />
-					</div>
-				</div>
-				<div className="posting_map_wrap">
-					<PostingMap setSearchMapModal={setSearchMapModal} data={data} />
-					{searchMapModal && (
-						<SearchPlace
-							setSearchMapModal={setSearchMapModal}
-							setData={setData}
-							data={data}
-						/>
-					)}
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+            name="content"
+            value={data.content}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div className="posting_calendar_wrap">
+          <div className="posting_calendar_icon">
+            <Calendar setData={setBlockDateDtoList} data={blockDateDtoList} />
+          </div>
+        </div>
+        <div className="posting_map_wrap">
+          <PostingMap setSearchMapModal={setSearchMapModal} data={data} />
+          {searchMapModal && (
+            <SearchPlace
+              setSearchMapModal={setSearchMapModal}
+              setData={setData}
+              data={data}
+            />
+          )}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Posting;
