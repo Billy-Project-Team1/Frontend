79dd7a2554234eb55f6c435d5f095afae59e9717
Yoonwon <blacksyw@naver.com>
2022-09-20 21:31:34 +0900

    🚧WIP: Firebase 알림 기능 구현중
    🚧WIP: Firebase 알림 기능 구현중

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Detail from './pages/detail/Detail';
@@ -16,27 +16,57 @@ import Search from './pages/search/Search';
 import ChattingRoom from './pages/chatting/ChattingRoom';
 import Chat from './pages/chatting/Chat';
 import ReviewPosting from './pages/reviewPosting/ReviewPosting';
+import { onMessageListener } from './firebaseInit';
+import Notifications from './commponents/notifications/Notifications';
+import ReactNotificationComponent from './commponents/notifications/ReactNotification';
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState({ title: '', body: '' });
+
+  console.log(show, notification);
+
+  onMessageListener()
+    .then((payload) => {
+      setShow(true);
+      setNotification({
+        title: payload.notification.title,
+        body: payload.notification.body,
+      });
+      console.log(payload);
+    })
+    .catch((err) => console.log('failed: ', err));
+
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/posting" element={<Posting />} />
-      <Route path="/modifyPosting" element={<ModifyPosting />} />
-      <Route path="/modifyProfile" element={<ModifyProfile />} />
-      <Route path="/detail/:postid" element={<Detail />} />
-      <Route path="/kakao" element={<Kakao />} />
-      <Route path="/map/test" element={<SearchPlace />} />
-      <Route path="/main/test" element={<MainListCard />} />
-      <Route path="/mypage/:id" element={<MyPage />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/chatList" element={<ChattingRoom />} />
-      {/* <Route path="/chat/:roomId" element={<Chat />} /> */}
-      <Route path="/chat/room/:postId/:roomId" element={<Chat />} />
-      <Route path="/reviewPosting" element={<ReviewPosting />} />
-    </Routes>
+    <div>
+      {show ? (
+        <ReactNotificationComponent
+          title={notification.title}
+          body={notification.body}
+        />
+      ) : (
+        <></>
+      )}
+      <Notifications />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/posting" element={<Posting />} />
+        <Route path="/modifyPosting" element={<ModifyPosting />} />
+        <Route path="/modifyProfile" element={<ModifyProfile />} />
+        <Route path="/detail/:postid" element={<Detail />} />
+        <Route path="/kakao" element={<Kakao />} />
+        <Route path="/map/test" element={<SearchPlace />} />
+        <Route path="/main/test" element={<MainListCard />} />
+        <Route path="/mypage/:id" element={<MyPage />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/chatList" element={<ChattingRoom />} />
+        {/* <Route path="/chat/:roomId" element={<Chat />} /> */}
+        <Route path="/chat/room/:postId/:roomId" element={<Chat />} />
+        <Route path="/reviewPosting" element={<ReviewPosting />} />
+      </Routes>
+    </div>
   );
 }
 
