065f2067112f19b8eb5ba3a5fe4f9ba3e1f62022
Eunkyoung Kim <gina.kim.4130@gmail.com>
2022-09-22 04:31:19 +0900

    ✨Feat: 마이페이지 새로고침 수정 완료
    ✨Feat: 마이페이지 새로고침 수정 완료

--- src/commponents/reservations/CancelPage.jsx
+++ src/commponents/reservations/CancelPage.jsx
@@ -4,8 +4,16 @@ import dailycost from '../../static/image/dailycost.svg';
 import deposit from '../../static/image/deposit.svg';
 import { HiOutlineChevronLeft } from 'react-icons/hi';
 
-const CancelPage = ({ setModalOpen,title,dailyPrice,img }) => {
-
+const CancelPage = ({
+  setModalOpen,
+  title,
+  dailyPrice,
+  depositPrice,
+  img,
+  startDate,
+  endDate,
+  totalAmount,
+}) => {
   const closeModal = () => {
     setModalOpen(false);
   };
@@ -13,20 +21,20 @@ const CancelPage = ({ setModalOpen,title,dailyPrice,img }) => {
   return (
     <div className="cancelpage_modal">
       <div className="cancelpage_wrap">
-        <div className='cancelpage_header_wrap'>
+        <div className="cancelpage_header_wrap">
           <div className="cancelpage_header_content">
             <HiOutlineChevronLeft
               color="#212121"
               size="24px"
-              onClick={()=>closeModal()}
+              onClick={closeModal}
             />
             <div className="cancelpage_header_title">대여 예약 취소</div>
           </div>
         </div>
         <div className="cancelpage_container">
-          <img className="cancelpage_img"src={img}/>
+          <img className="cancelpage_img" src={img} />
           <div className="cancelpage_content_box">
-            <div className='cancelpage_title'>{title}</div>
+            <div className="cancelpage_title">{title}</div>
             <div className="cancelpagae_price_wrap">
               <div className="cancelpage_price_box">
                 <img className="cancelpage_icon" src={dailycost} />
@@ -34,10 +42,10 @@ const CancelPage = ({ setModalOpen,title,dailyPrice,img }) => {
               </div>
               <div className="cancelpage_price_box">
                 <img className="cancelpage_icon" src={deposit} />
-                <p>490000</p>
+                <p>{depositPrice}</p>
               </div>
             </div>
-            <div>날짜</div>
+            <div className="cancelpage_date">날짜</div>
           </div>
         </div>
         <div className="cancelpage_body">취소 사유</div>
