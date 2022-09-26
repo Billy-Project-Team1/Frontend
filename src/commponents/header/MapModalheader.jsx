fbee83a7bf21890de01816e4ac8d582b0c4a9a76
Yoonwon <blacksyw@naver.com>
2022-09-27 02:02:46 +0900

    🚧WIP: GPS기능 추가
    🚧WIP: GPS기능 추가

---
+++ src/commponents/header/MapModalheader.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Headers.scss';
+import { useNavigate } from 'react-router-dom';
+import { HiOutlineChevronLeft } from 'react-icons/hi';
+
+const MapModalheader = ({ pageName, setSearchMapModal }) => {
+	const navigate = useNavigate();
+
+	return (
+		<div className="header_container">
+			<div className="header_wrap">
+				<div className="mypage_header_content">
+					<HiOutlineChevronLeft
+						style={{ marginRight: '17px' }}
+						color="#212121"
+						size="24px"
+						onClick={() => setSearchMapModal(false)}
+					/>
+					<div className="mypage_header_title">{pageName}</div>
+					<div className="mypage_header_done">&nbsp;</div>
+				</div>
+				<div className="header_line"></div>
+			</div>
+		</div>
+	);
+};
+
+export default MapModalheader;
