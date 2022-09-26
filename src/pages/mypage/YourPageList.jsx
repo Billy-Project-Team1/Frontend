cdef7d9240a60e5f03c01946301467b85585a172
4775614 <4775614@naver.com>
2022-09-26 23:52:40 +0900

    🚧WIP:Review CRUD 구현 중

--- src/pages/mypage/YourPageList.jsx
+++ src/pages/mypage/YourPageList.jsx
@@ -5,28 +5,32 @@ import {  useSelector } from 'react-redux';
 // Component import
 import YourPageMiddle from '../../commponents/myProfile/YourPageMiddle';
 import UploadCard from '../../commponents/profileCard/UploadCard';
-import ReviewCard from '../../commponents/Review/ReviewCard';
+import ReviewCardYourPage from '../../commponents/Review/ReviewCardYourPage';
 
-const YourPageList = () => {
+const YourPageList = ({profile}) => {
   const [yourPageState, setYourPageState] = useState('1');
   const myUploadList = useSelector((state) => state.member.myUploadList);
   console.log(myUploadList)
 
   return (
-    <div>
-      <YourPageMiddle
-        yourPageState={yourPageState}
-        setYourPageState={setYourPageState}
-      />
-      {yourPageState === '1' ? (
-        <div className="mypage_list_margin">
-          {myUploadList.map((post,index) => {
-            return <UploadCard post={post} key={index}/>;
-          })}
-        </div>
-      ) : yourPageState ==='2'?(<ReviewCard />):('')}
-    </div>
-  );
+		<div>
+			<YourPageMiddle
+				yourPageState={yourPageState}
+				setYourPageState={setYourPageState}
+			/>
+			{yourPageState === '1' ? (
+				<div className="mypage_list_margin">
+					{myUploadList.map((post, index) => {
+						return <UploadCard post={post} key={index} />;
+					})}
+				</div>
+			) : yourPageState === '2' ? (
+				<ReviewCardYourPage profile={profile} />
+			) : (
+				''
+			)}
+		</div>
+	);
 };
 
 export default YourPageList;
