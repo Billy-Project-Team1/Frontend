11ea28b83a19d50a040d9efe655434d3083c2cca
Eunkyoung Kim <gina.kim.4130@gmail.com>
2022-09-28 01:23:25 +0900

    ✨Feat: 리뷰 수정/삭제 모달 추가
    ✨Feat: 리뷰 수정/삭제 모달 추가

--- src/commponents/Review/ReviewCardContent.jsx
+++ src/commponents/Review/ReviewCardContent.jsx
@@ -1,74 +1,93 @@
 // React import
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 // Style import
 import './ReviewCard.scss';
 import ReviewStarRating from '../starRating/ReviewStarRating';
 import { TbMinusVertical } from 'react-icons/tb';
 import { TbDotsVertical } from 'react-icons/tb';
+import ReviewModal from '../modal/ReviewModal';
 
-const ReviewCardContent = ({ item, index }) => {
-	function rentalDate(a) {
-		var d = new Date(a);
-		return (
-			(d.getMonth() + 1 > 9
-				? (d.getMonth() + 1).toString()
-				: '0' + (d.getMonth() + 1)) +
-			'.' +
-			(d.getDate() > 9
-				? d.getDate().toString()
-				: '0' + d.getDate().toString())
-		);
-	}
+const ReviewCardContent = ({ item, index, authorId }) => {
+  const userId = localStorage.getItem('userId');
 
-	const textRef = useRef();
-	const handleResizeHeight = useCallback(() => {
-		textRef.current.style.height = textRef.current.scrollHeight + 'px';
-	}, []);
+  function rentalDate(a) {
+    var d = new Date(a);
+    return (
+      (d.getMonth() + 1 > 9
+        ? (d.getMonth() + 1).toString()
+        : '0' + (d.getMonth() + 1)) +
+      '.' +
+      (d.getDate() > 9 ? d.getDate().toString() : '0' + d.getDate().toString())
+    );
+  }
 
-	return (
-		<div>
-			<div className="reviewCard_wrap_2" key={index}>
-				<div className="reviewCard_title">{item.title}</div>
-				<div className="reviewCard_rating_name_wrap">
-					<div className="reviewCard_rating_name_box">
-						<ReviewStarRating
-							className="reviewStarRating_star"
-							reviewRating={item.star}
-						/>
-					</div>
-					<div className="reviewCard_rating_container">
-						<div className="reviewCard_rating_box">
-							<div className="reviewCard_rating">{item.star}.0</div>
-							<div className="reviewCard_rating_name_box">
-								<div className="reviewCard_rating_box">
-									<div className="reviewCard_rental_name">{item.nickname}</div>
-									<div className="reviewCard_middle_line">
-										<TbMinusVertical />
-									</div>
-									<div className="reviewCard_rental_date">
-										{rentalDate(item.startDate)} - {rentalDate(item.endDate)} (
-										{item.dateCount}박)
-										<TbDotsVertical />
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="reviewCard_imgs_wrap">
-					{item.reviewImgUrl.map((img, index) => {
-						return (
-							<div className="reviewCard_img_list" key={index}>
-								<img className="reviewCard_img" src={img.reviewImgUrl} alt="" />
-							</div>
-						);
-					})}
-				</div>
-				<div className="reviewCard_comment">{item.comment}</div>
-			</div>
-		</div>
-	);
+  const textRef = useRef();
+  const handleResizeHeight = useCallback(() => {
+    textRef.current.style.height = textRef.current.scrollHeight + 'px';
+  }, []);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const showModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  return (
+    <div>
+      <div className="reviewCard_wrap_2" key={index}>
+        <div className="reviewCard_title">{item.title}</div>
+        <div className="reviewCard_rating_name_wrap">
+          <div className="reviewCard_rating_name_box">
+            <ReviewStarRating
+              className="reviewStarRating_star"
+              reviewRating={item.star}
+            />
+          </div>
+          <div className="reviewCard_rating_container">
+            <div className="reviewCard_rating_box">
+              <div className="reviewCard_rating">{item.star}.0</div>
+              <div className="reviewCard_rating_name_box">
+                <div className="reviewCard_rating_box">
+                  <div className="reviewCard_rental_name">{item.nickname}</div>
+                  <div className="reviewCard_middle_line">
+                    <TbMinusVertical />
+                  </div>
+                  <div className="reviewCard_rental_date">
+                    {rentalDate(item.startDate)} - {rentalDate(item.endDate)} (
+                    {item.dateCount}박)
+                    {userId === authorId ? (
+                      <>
+                        <TbDotsVertical onClick={() => showModal()} />
+                      </>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="reviewCard_imgs_wrap">
+          {item.reviewImgUrl.map((img, index) => {
+            return (
+              <div className="reviewCard_img_list" key={index}>
+                <img className="reviewCard_img" src={img.reviewImgUrl} alt="" />
+              </div>
+            );
+          })}
+        </div>
+        <div className="reviewCard_comment">{item.comment}</div>
+      </div>
+      {modalOpen && (
+        <ReviewModal
+          showModal={showModal}
+          setModalOpen={setModalOpen}
+          //   postId={postId}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ReviewCardContent;
---
+++ src/commponents/modal/ReviewModal.jsx
@@ -0,0 +1,61 @@
+// React import
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// Redux import
+import { useDispatch } from 'react-redux';
+// Style import
+import '../../commponents/header/DetailModal.scss';
+// Component import
+import AlertSmallModal from '../modal/AlertSmallModal';
+
+const ReviewModal = ({ showModal, postId }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [modalOn, setModalOn] = useState(false);
+  const modalTrue = () => {
+    setModalOn(true);
+  };
+
+//   const deleteHandler = async () => {
+//     try {
+//       const response = await dispatch(deletePost(postId)).unwrap();
+//       if (response) {
+//         window.location.replace('/');
+//       }
+//     } catch {}
+//   };
+
+  return (
+    <div className="detailModal_background">
+      <div className="detailModal_container">
+        <div className="detailModal_btn_first">
+          <div
+            className="detailModal_edit_btn"
+            onClick={() => navigate(`/modifyPosting/${postId}`)}
+          >
+            댓글 수정하기
+          </div>
+          <div className="detailModal_del_btn" onClick={modalTrue}>
+            삭제
+          </div>
+          {modalOn&& (
+            <AlertSmallModal
+              setModalOn={setModalOn}
+              body="삭제하시겠습니까?"
+              buttonType="삭제"
+            //   onClickSubmit={deleteHandler}
+            />
+          )}
+        </div>
+        <div className="detailModal_btn_second">
+          <div className="detailModal_cancel_btn" onClick={showModal}>
+            취소
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewModal;
